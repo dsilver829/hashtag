9fa76fcdab94efd21e50aacd21523a5def8f0a0a
David Silver <dsilver829@aol.com>
2013-10-22 12:32:27 -0700

    ensure that tweets appear

--- spec/features/search_twitter_spec.rb
+++ spec/features/search_twitter_spec.rb
@@ -5,5 +5,6 @@ feature 'Search Twitter for a term' do
     visit root_path
     fill_in 'Search', with: 'Rails'
     click_button 'Search'
+    expect(page).to have_css 'li.tweet', count: 15
   end
 end
